1e833d5f8ebf8f31ca2943855b6afaeea131f827
Miranda Breves <brevmira@isu.edu>
2022-03-07 09:11:20 -0700

    Add project files.

---
+++ DinerMenu/DinerMenuForm.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DinerMenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Niagara Solid", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(413, 18)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(672, 115)
+        Me.DinerNameLabel.TabIndex = 0
+        Me.DinerNameLabel.Text = "California Dinin' Specials"
+        Me.DinerNameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.SoupButton)
+        Me.GroupBox1.Controls.Add(Me.SaladButton)
+        Me.GroupBox1.Controls.Add(Me.FishButton)
+        Me.GroupBox1.Controls.Add(Me.ExitButton)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 741)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1468, 143)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(24, 30)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(208, 93)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(253, 30)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(208, 93)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(482, 30)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(208, 93)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(1237, 30)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(208, 93)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Niagara Solid", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(12, 133)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(980, 605)
+        Me.DisplaySpecialLabel.TabIndex = 2
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(1012, 133)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(468, 304)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(1012, 443)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(468, 292)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(1492, 896)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Name = "DinerMenuForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "California Dinin' Special Menu"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DinerNameLabel As Windows.Forms.Label
+    Friend WithEvents GroupBox1 As Windows.Forms.GroupBox
+    Friend WithEvents SoupButton As Windows.Forms.Button
+    Friend WithEvents SaladButton As Windows.Forms.Button
+    Friend WithEvents FishButton As Windows.Forms.Button
+    Friend WithEvents ExitButton As Windows.Forms.Button
+    Friend WithEvents DisplaySpecialLabel As Windows.Forms.Label
+    Friend WithEvents PictureBox1 As Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As Windows.Forms.PictureBox
+End Class
---
+++ DinerMenu/DinerMenuForm.vb
@@ -0,0 +1,60 @@
+﻿'Miranda Breves
+'RCET0265
+'Spring 2022
+'Diner Menu Program
+'github url
+
+Option Strict On
+Option Explicit On
+
+Public Class DinerMenuForm
+    Private Sub DinerMenuForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DisplaySpecialLabel.Text = ""
+    End Sub
+
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+
+        DisplaySpecialLabel.Text = "Monday: Broccoli Cheddar Soup and Salad" &
+                                   vbNewLine & "Tuesday: Minestrone with Rolls" &
+                                   vbNewLine & "Wednesday: Potato Soup with Bacon Bits and Rolls" &
+                                   vbNewLine & "Thursday: Tomato-Basil Soup and Salad" &
+                                   vbNewLine & "Friday: Clam Chowder in a Breadbowl" &
+                                   vbNewLine & "Saturday: Gazpacho Soup with Quesidillas" &
+                                   vbNewLine & vbNewLine & "Sunday: Unfortunately, we are closed."
+
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+
+        DisplaySpecialLabel.Text = "Our Salads" &
+                                   vbNewLine & StrDup(150, "-") &
+                                   vbNewLine & "Caesar Salad" &
+                                   vbNewLine & "Cobb Salad" &
+                                   vbNewLine & "Chicken Garden Salad" &
+                                   vbNewLine & "Grilled Shrimp Salad with Avocado" &
+                                   vbNewLine & "Greek Salad" &
+                                   vbNewLine & "Pasta Salad" &
+                                   vbNewLine & "Ambrosia"
+
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+
+        DisplaySpecialLabel.Text = "Fish Specials" &
+                                   vbNewLine & StrDup(150, "-") &
+                                   vbNewLine & "Crispy Calimari with Tempura" &
+                                   vbNewLine & "Panko-breaded and fried squid with tempura battered " &
+                                   "onions, sweet potato, broccoli, carrots, " &
+                                   vbNewLine & "zucchini and dipping sauce." &
+                                   vbNewLine & vbNewLine & "Glazed Salmon with Mango Salad" &
+                                   vbNewLine & "A sweet and tangy pinapple flavored glazed salmon cut " &
+                                   "with a mango salad containing mango " &
+                                   vbNewLine & " slices, almonds, and a sweet dressing."
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
